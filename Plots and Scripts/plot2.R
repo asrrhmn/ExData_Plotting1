4e73701293c482d56ec56d6acc4fc6f3dfe99e7d
Anis Rahman <34162137+asrrhmn@users.noreply.github.com>
2018-03-12 12:25:37 +0530

    updated script with auto file download code

--- Plots and Scripts/plot2.R
+++ Plots and Scripts/plot2.R
@@ -1,31 +1,32 @@
-#Load required Libraries
-library(dplyr)
-
-# get required local path
-lpath <- getwd()
-#Download URL for source dataset ( the file if zipped to save network bandwidth)
-URL <-"https://drive.google.com/file/d/1I4FGLXRiGQgMszrCSEg9yaX0fYYIztMt/view?usp=sharing"
-
-if (!file.exists("household_power_consumption.txt")){
-  download.file(URL,destfile = "power_cons_ds.7z")
-  unzip("power_cons_ds.zip")
-  #retrieve the file using read.delim as we are working with
-  #delimited text file with delimited symbol as ";"
-  powerdf <- read.delim(file.path(lpath,"/household_power_consumption.txt")
-                        ,sep=";",na.strings = "?")
-  print("Data set downloaded and is ready to use")
-}else {
-  #retrieve the file using read.delim as we are working with
-  #delimited text file with delimited symbol as ";"
-  powerdf <- read.delim(file.path(lpath,"/household_power_consumption.txt")
-                        ,sep=";",na.strings = "?")
-  print("Data set already exist and is ready to use")
-}
 
 #create new column DateTime
 powerdf$DateTime <- as.POSIXct(paste(powerdf$Date, powerdf$Time), 
                                format = "%d/%m/%Y %H:%M:%S")
+URL <-"https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
 
+if (!file.exists("household_power_consumption.txt")){
+  print("Download in progress")
+  download.file(URL,destfile = "power_cons_ds.zip")
+  print("Download complete .....")
+  print("File Unzip in progress .....")
+  unzip('power_cons_ds.zip')
+  print("Unzip complete, file should reside in Working directory")
+  #retrieve the file using read.delim as we are working with
+  #delimited text file with delimited symbol as ";"
+  print("data set in text file is being read into data frame powerdf, execution in progress....")
+  powerdf <- read.delim(file.path(lpath,"/household_power_consumption.txt")
+                        ,sep=";",na.strings = "?")
+  print("execution completed")
+  print("Data set downloaded and is ready to use")
+}else {
+  #retrieve the file using read.delim as we are working with
+  #delimited text file with delimited symbol as ";"
+  print("data set in text file is being read into data frame powerdf, execution in progress....")
+  powerdf <- read.delim(file.path(lpath,"/household_power_consumption.txt")
+                        ,sep=";",na.strings = "?")
+  print("execution completed")
+  print("Data set and is ready to use")
+}
                       
 #filter for dates 2007-02-01 and 2007-02-02
 powerf2 <- powerdf%>% filter(DateTime>="2007-02-01" & DateTime < "2007-02-03")
